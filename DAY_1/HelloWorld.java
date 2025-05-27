5b8cf32e8f950b5d25dae31e061c27fb24325241
Surya-Kaliappan <kaliappansurya47@gmail.com>
2025-05-28 04:25:24 +1200

    Day 1 code Added

--- DAY_1/HelloWorld.java
+++ DAY_1/HelloWorld.java
@@ -1,7 +1,7 @@
 // Just print the command line inputs.
 
 public class HelloWorld{
-  static int a = 20;
+  static int a = 10;
   public static void main(String[] args){
     System.out.println(a);
   }
