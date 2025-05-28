9fb7e4eba51814c17de8691ef1634c01f83d705e
Surya-Kaliappan <kaliappansurya47@gmail.com>
2025-05-28 21:08:25 +1200

    Day 1 codes

---
+++ DAY_1/HomeWork.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HomeWork{
+
+    public static boolean withModulus(int value){
+        return value%2==0;
+    }
+
+    public boolean withoutModulus(int value){
+        return (value&1)==0 ;
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Value : ");
+        int value = input.nextInt();
+        System.out.println("With Modulus Answer : "+(withModulus(value) ? "Even" : "Odd"));
+        System.out.println("Without Modulus Answer : "+(withoutModulus(value) ? "Even" : "Odd"));
+    }
+
+    
+}
