4b48fb6dba9344da24d7bf2e7cc10fe255005d14
Surya-Kaliappan <kaliappansurya47@gmail.com>
2025-06-03 14:15:22 +1200

    Day 7 codes

---
+++ DAY_7/ArrayProblem.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArrayProblem{
+
+    static void ReverseArray(int[] arr, int start, int end){
+        int temp;
+        while(start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void RotateArrayToLeft(int[] arr, int n){
+        int len = arr.length;
+        n = n%len;
+        ReverseArray(arr,0,n-1);
+        ReverseArray(arr,n,len-1);
+        ReverseArray(arr,0,len-1);
+
+        // int[] B = new int[len];
+        // for(int i=0;i<len;i++){
+        //     B[i] = arr[(i+n)%len];
+        // }
+        // return B;
+    }
+
+    static void RotateArrayToRight(int[] arr,int n){
+        int len = arr.length;
+        n = n%len;
+        ReverseArray(arr,0,len-1);
+        ReverseArray(arr,0,n-1);
+        ReverseArray(arr,n,len-1);
+    }
+
+    static boolean CheckArraySort(int[] arr){
+        boolean check = true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]<arr[i+1]){
+                check = false;
+            }
+        }
+        if(check) return check;
+        check = true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                check = false;
+            }
+        }
+        return check;
+    }
+
+    static boolean checkTask(int[] a){
+        // return arr[0]<arr[arr.length-1];
+        int n = a.length;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(a[i]>a[(i+1)%n]){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+        return cnt<=1;
+    }
+
+    static void KeepZeroAtLast(int[] arr){
+        int i = 0,t;
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=0){
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void FindFrequencyOfArray(int[] arr){
+        int len = arr.length,count;
+        boolean[] check = new boolean[len];
+        for(int i=0;i<len;i++){
+            if(!check[i]){
+                count = 0;
+                for(int j=i;j<len;j++){
+                    if(arr[i]==arr[j]){
+                    count++;
+                    check[j] = true;
+                    }
+                }
+                System.out.println(arr[i]+" -> "+count);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int[] arr = {1,2,0,4,5,1,2,3,4};
+        // System.out.println(checkTask(arr));
+        // RotateArrayToRight(arr,4);
+        // System.out.println(checkTask(arr));
+        FindFrequencyOfArray(arr);
+    }
+}
---
+++ DAY_7/Notes.md
@@ -0,0 +1,4 @@
+# Array Problems
+
+1) Check if ther Given Array is Sorted and Rotated.
+2) Write a program to find the frequency of the given array.
