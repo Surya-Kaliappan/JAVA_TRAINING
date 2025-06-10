fd60eb05589497f0a99a6cb46a904f9cb4977138
Surya-Kaliappan <kaliappansurya47@gmail.com>
2025-06-10 21:16:19 +1200

    Day 12 code HW

--- DAY_12/Vowels.java
+++ DAY_12/Vowels.java
@@ -1,14 +1,30 @@
 public class Vowels{
-
-    static findVowels(String s){
-        int[] arr = new int[s.length()];
+    static String findVowels(String s){
         StringBuilder sb = new StringBuilder(s);
-        for(int i=0;i<sb.length;i++){
-            if()
+        String vowels = "aeiouAEIOU", check1, check2;
+        int i=0,j=sb.length()-1;
+        while(i<j){
+            check1 = String.valueOf(sb.charAt(i));
+            check2 = String.valueOf(sb.charAt(j));
+            if(vowels.contains(check1) && vowels.contains(check2)){
+                char temp = sb.charAt(i);
+                sb.setCharAt(i,sb.charAt(j));
+                sb.setCharAt(j,temp);
+                i++;
+                j--;
+            } else {
+                if(!vowels.contains(check1)){
+                    i++;
+                }
+                if(!vowels.contains(check2)){
+                    j--;
+                }
+            }
         }
+        return sb.toString();
     }
 
     public static void main(String[] args){
-        
+        System.out.println(findVowels("Zoho Corporation"));
     }
 }
